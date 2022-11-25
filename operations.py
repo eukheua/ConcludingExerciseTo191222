021f008afde0c4a5e95ad00fb7ac8f5d45f898b9
SHAFIR-MAIN <shafir.ohad@gmail.com>
2022-11-25 13:38:36 +0200

    building the arithmetical functions

--- operations.py
+++ operations.py
@@ -1,42 +1,78 @@
-def main():
-    operations_order_dict = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 4, "$": 5, "&": 5, "@": 5, "~": 6, "!": 6}
+import math
 
-    class Operator(object):
-        def __init__(self, symbol, order_in_operations, position):
-            self.symbol = symbol
-            self.order_in_operations = order_in_operations
-            self.position = position
 
-        def is_valid_symbol(self, symbol):
-            if symbol not in "+-*/^%$&@~!":
-                raise ValueError("Not valid operator")
-            return symbol
-        def is_valid_symbol_order_in_operations(self, order_in_operations):
-            if order_in_operations not in range(1,7):
-                raise ValueError("Not valid order_in_operations")
-            return order_in_operations
-        def is_valid_position(self, position):
-            if position != "left" and position != "right" and position != "middle" :
-                raise ValueError("Not valid position")
-            return position
+def is_valid_symbol(symbol):
+    if symbol not in "+-*/^%$&@~!":
+        raise ValueError("Not valid operator")
+    return symbol
 
-        def get_first_name(self):
-            return self.first_name
 
-        def get_last_name(self):
-            return self.last_name
+def is_valid_symbol_order_in_operations(order_in_operations):
+    if order_in_operations not in range(1, 7):
+        raise ValueError("Not valid order_in_operations")
+    return order_in_operations
 
-        def get_age(self):
-            return self.age
 
-        def set_first_name(self, first_name):
-            self.first_name = first_name
+def is_valid_position(position):
+    if position != "left" and position != "right" and position != "middle":
+        raise ValueError("Not valid position")
+    return position
 
-        def set_last_name(self, last_name):
-            self.last_name = last_name
 
-        def set_age(self, age):
-            self.age = age
+class Operator(object):
+    def __init__(self, symbol, order_in_operations, position):
+        self.symbol = is_valid_symbol(symbol)
+        self.order_in_operations = is_valid_symbol_order_in_operations(order_in_operations)
+        self.position = position
 
-if __name__ == "__main__":
-    main()
+
+def add(op1, op2):
+    return op1 + op2
+
+
+def sub(op1, op2):
+    return op1 - op2
+
+
+def mul(op1, op2):
+    return op1 * op2
+
+
+def div(op1, op2):
+    return op1 / op2
+
+
+def power(op1, op2):
+    return math.pow(op1, op2)
+
+
+def mod(op1, op2):
+    return op1 % op2
+
+
+def maximum(op1, op2):
+    if op1 > op2:
+        return op1
+    else:
+        return op2
+
+
+def minimum(op1, op2):
+    if op1 < op2:
+        return op1
+    else:
+        return op2
+
+
+def avg(op1, op2):
+    return (op1 + op2) / 2
+
+
+def neg(op):
+    return -op
+
+
+def fac(op):
+    if op == 1 or op == 0:
+        return 1
+    return fac(op-1) * op
